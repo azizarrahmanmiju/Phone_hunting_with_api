c89fca7f4445846d25e7621abb307ebd7fb035af
azizarrahman <azizarrahman01032003@gmail.com>
2024-11-04 21:36:37 +0600

    search feature done

--- script/app.js
+++ script/app.js
@@ -1,21 +1,21 @@
-console.log('connected');
 
-const loadData = async () => {
-    const res = await fetch('https://openapi.programming-hero.com/api/phones?search=iphone');
-    const data = await res.json();
-    const phones = data.data;
-    showInDisplay(phones);
-    // console.log(data);
+const loadData = async (value = 'apple') => {
+  const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${value}`);
+  const data = await res.json();
+  const phones = data.data;
+  showInDisplay(phones);
+  // console.log(data);
 }
 
 const showInDisplay = phones => {
-    const phoneContainer = document.getElementById('phone-container');
-    phones.forEach(value => {
-        console.log(value)
+  const phoneContainer = document.getElementById('phone-container');
+  phoneContainer.innerHTML = '';
+  phones.forEach(value => {
+    console.log(value)
 
-        const phonediv = document.createElement('div');
-        phonediv.className = 'card bg-base-100 w-auto shadow-xl bg-red grid';
-        phonediv.innerHTML = `
+    const phonediv = document.createElement('div');
+    phonediv.className = 'card bg-base-100 m-2 flex-1 shadow-xl';
+    phonediv.innerHTML = `
         <figure>
     <img
       src="${value.image}"
@@ -25,18 +25,23 @@ const showInDisplay = phones => {
     <h2 class="card-title">${value.brand}</h2>
     <p>${value.phone_name}</p>
     <div class="card-actions justify-end">
-      <button class="btn btn-primary w-full">Buy Now</button>
+      <button class="btn btn-primary ">Buy Now</button>
     </div>
   </div>
-                  
-        `
+                `;
 
+    phoneContainer.appendChild(phonediv);
 
-        phoneContainer.appendChild(phonediv);
+  });
 
-    });
+}
 
+const searchbarclick = () => {
+  const serchvalue = document.getElementById('sechbar').value;
+  loadData(serchvalue)
+  console.log(serchvalue);
 }
 
 
 loadData();
+
